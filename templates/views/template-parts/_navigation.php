a0ea0c8e281fefe40efcbdb9d280eb8f4c1cf0c3
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-07-23 12:35:21 +0200

    main nav margin

--- templates/views/template-parts/_navigation.php
+++ templates/views/template-parts/_navigation.php
@@ -2,8 +2,8 @@
 // Get home page and home childrens
 $root = pages('/')->and(pages('/')->children);
 ?>
-<div class='uk-margin-top uk-margin-bottom uk-overflow-auto'>
-  <ul class="uk-subnav uk-flex uk-flex-nowrap">
+<div class='uk-overflow-auto'>
+  <ul class="uk-subnav uk-flex uk-flex-nowrap uk-margin-top uk-margin-bottom">
     <?php foreach ($root as $item):
       $class = $item->id == wire('page')->id ? 'uk-active active' : 'basic-link';
     ?>
