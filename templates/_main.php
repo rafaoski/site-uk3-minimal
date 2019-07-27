fcf5fc419f359bc18f9b96a205a10fa0d9d4fecf
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-07-27 13:53:51 +0200

    cleanup

--- templates/_main.php
+++ templates/_main.php
@@ -20,92 +20,78 @@ if(page()->images && count(page()->images)) {
 	echo siteHead();
 ?>
 </head>
-
 <body id='html-body' class='html-body <?= setting('body-classes')->implode(' ') ?>'<?= backgroundImage(['img' => $img]) ?>>
 
-<!-- HEADER -->
-<header id='header' class='header uk-panel'>
+	<!-- HEADER -->
+	<header id='header' class='header uk-panel'>
 
-	<!-- PRIVACY POLICY-->
-	<?= privacyPolicy(['class' => 'privacy-policy uk-padding-small uk-flex uk-flex-wrap uk-flex-left']) ?>
+		<!-- PRIVACY POLICY-->
+		<?= privacyPolicy(['class' => 'privacy-policy uk-padding-small uk-flex uk-flex-wrap uk-flex-left']) ?>
 
-	<?= files()->render('views/template-parts/_language-menu.php') // Language menu ?>
+		<?= files()->render('views/template-parts/_language-menu.php') // Language menu ?>
 
-	<!-- LOGO-->
-	<?= siteLogo(['class' => 'logo uk-flex uk-flex-center']) ?>
+		<!-- LOGO-->
+		<?= siteLogo(['class' => 'logo uk-flex uk-flex-center']) ?>
 
-	<!-- SITE NAME -->
-	<?= siteName(['class' => 'site-name uk-text-uppercase uk-heading-small uk-margin-remove uk-text-center']) ?>
+		<!-- SITE NAME -->
+		<?= siteName(['class' => 'site-name uk-text-uppercase uk-heading-small uk-margin-remove uk-text-center']) ?>
 
-	<!-- BREADCRUMB -->
-	<?php if(page()->parent->id > setting('home')->id) echo ukBreadcrumb(page(),
-		[
-			'class' => 'uk-float-right uk-padding-small uk-visible@m',
-			'appendCurrent' => true
-		]);
-	?>
+		<!-- BREADCRUMB -->
+		<?php if(page()->parent->id > setting('home')->id) echo ukBreadcrumb(page(),
+			[
+				'class' => 'uk-float-right uk-padding-small uk-visible@m',
+				'appendCurrent' => true
+			]);
+		?>
 
-	<!-- NAVIGATION  -->
-	<?= files()->render('views/template-parts/_navigation.php', ['id' => 'nav' , 'class' => 'nav uk-container']) ?>
+		<!-- NAVIGATION  -->
+		<?= files()->render('views/template-parts/_navigation.php', ['id' => 'nav' , 'class' => 'nav uk-container']) ?>
 
-</header>
+	</header>
 
-<!-- HERO -->
-<div id="hero" class='hero uk-flex uk-flex-center uk-flex-middle uk-text-center uk-padding-small' data-uk-grid>
+	<!-- HERO -->
+	<div id="hero" class='hero uk-flex uk-flex-center uk-flex-middle uk-text-center uk-padding-small' data-uk-grid>
 
-	<?php
-		echo files()->render('views/template-parts/_hero-content.php',
-		[
-			'img' => $img,
-			'img_alt' => $img_alt
-		])
-	?>
+		<?php
+			echo files()->render('views/template-parts/_hero-content.php',
+			[
+				'img' => $img,
+				'img_alt' => $img_alt
+			])
+		?>
 
-</div>
-
-<!-- CONTENT -->
-<div id="content-body" class='content-body uk-container uk-container-medium uk-margin-top uk-margin-bottom'>
-	<?= page()->body ?>
-</div>
-
-<?= editPage() ?>
-<?= debugInfo() ?>
-
-<!-- FOOTER -->
-<footer id='footer' class="footer uk-section uk-section-small uk-margin-large-top">
-
-	<a id="to-top" title='to-top' class='to-top uk-float-right uk-padding uk-text-danger' href="#" data-uk-totop data-uk-scroll></a>
-
-	<div id="search-form" class='search-form uk-container uk-margin-small-bottom uk-width-1-2@m'>
-		<?= files()->render('views/template-parts/_search-form.php') ?>
 	</div>
 
-	<p id='copy-text' class="copy-text uk-text-small uk-text-center uk-text-muted uk-padding-small">
-		<?php echo files()->render('views/template-parts/_footer-demo-copyright.php') ?>
-		<?php // echo files()->render('views/template-parts/_footer-copyright.php') ?>
-	</p>
+	<!-- CONTENT -->
+	<div id="content-body" class='content-body uk-container uk-container-medium uk-margin-top uk-margin-bottom'>
+		<?= page()->body ?>
+	</div>
 
-</footer>
+	<?= editPage() ?>
+	<?= debugInfo() ?>
 
-<!-- OFF CANVAS NAV -->
-<a id='offcanvas-toggle' aria-label='Off Canvas Menu' class='uk-position-top-right uk-position-fixed uk-butoon uk-button-secondary'
-   href="#off-overlay" data-uk-toggle><?= ukIcon('menu', 1.7) ?>
-</a>
+	<!-- FOOTER -->
+	<footer id='footer' class="footer uk-section uk-section-small uk-margin-large-top">
 
-<div id="off-overlay" data-uk-offcanvas="overlay: true; flip: true;">
-	<?php
-		echo files()->render('views/template-parts/_offcanvas-nav.php',
-		[
-			'socialProfiles' => socialProfiles(pages('options')->textarea)
-		]);
-	?>
-</div>
+		<a id="to-top" title='to-top' class='to-top uk-float-right uk-padding uk-text-danger' href="#" data-uk-totop data-uk-scroll></a>
+
+		<div id="search-form" class='search-form uk-container uk-margin-small-bottom uk-width-1-2@m'>
+			<?= files()->render('views/template-parts/_search-form.php') ?>
+		</div>
+
+		<p id='copy-text' class="copy-text uk-text-small uk-text-center uk-text-muted uk-padding-small">
+			<?php echo files()->render('views/template-parts/_footer-demo-copyright.php') ?>
+			<?php // echo files()->render('views/template-parts/_footer-copyright.php') ?>
+		</p>
+
+	</footer>
+
+	<?= files()->render('views/template-parts/_offcanvas-nav.php') ?>
 
 <?php
 // echo googleFonts( ['fonts' => ['Nunito:200,600','Butcherman']] ); // Google Fonts
 // echo gwCode( setting('gw-code') ); // Google Webmaster
 // echo gaCode( setting('ga-code') ); // Google Analytics
 ?>
-
 </body>
 </html>
--- templates/views/template-parts/_offcanvas-nav.php
+++ templates/views/template-parts/_offcanvas-nav.php
@@ -1,25 +1,34 @@
-<?php namespace ProcessWire ?>
+<?php namespace ProcessWire; ?>
 
-<div class="uk-offcanvas-bar">
+<!-- OFF CANVAS NAV -->
+<a id='offcanvas-toggle' aria-label='Off Canvas Menu' class='uk-position-top-right uk-position-fixed uk-butoon uk-button-secondary'
+	href="#off-overlay" data-uk-toggle><?= ukIcon('menu', 1.7) ?>
+</a>
 
-	<!-- SOCIAL PROFILES-->
-	<div id="social-profiles" class='social-profiles uk-flex uk-flex-center uk-padding-small'>
-		<?= $socialProfiles ?>
+<div id="off-overlay" data-uk-offcanvas="overlay: true; flip: true;">
+
+	<div class="uk-offcanvas-bar">
+
+		<!-- SOCIAL PROFILES-->
+		<div id="social-profiles" class='social-profiles uk-flex uk-flex-center uk-padding-small'>
+			<?= socialProfiles(pages('options')->textarea) ?>
+		</div>
+
+		<button class="uk-offcanvas-close" type="button" data-uk-close></button>
+		<h3 class='uk-h4'><a aria-label='Home' href='<?= setting('home')->url ?>'><?= pages('options')->site_name ?></a></h3>
+			<?php
+				// example of caching generated markup (for 600 seconds/10 minutes)
+				echo cache()->get('offcanvas-nav', 10, function() {
+				return ukNav(pages('/')->children(), [
+					'depth' => 2,
+					'accordion' => true,
+					// 'blockParents' => [ 'blog' ],
+					'repeatParent' => true,
+					'noNavQty' => 20,
+					'maxItems' => 16,
+				]);
+				});
+			?>
 	</div>
 
-	<button class="uk-offcanvas-close" type="button" data-uk-close></button>
-	<h3 class='uk-h4'><a aria-label='Home' href='<?= setting('home')->url ?>'><?= pages('options')->site_name ?></a></h3>
-		<?php
-			// example of caching generated markup (for 600 seconds/10 minutes)
-			echo cache()->get('offcanvas-nav', 10, function() {
-			return ukNav(pages('/')->children(), [
-				'depth' => 2,
-				'accordion' => true,
-				// 'blockParents' => [ 'blog' ],
-				'repeatParent' => true,
-				'noNavQty' => 20,
-				'maxItems' => 16,
-			]);
-			});
-		?>
 </div>
